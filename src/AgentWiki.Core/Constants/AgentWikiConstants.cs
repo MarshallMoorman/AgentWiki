5499ab0ca90529446be9f5e1c382903ed62ee55b
Marshall Moorman <mmoorman@elevate.com>
2026-07-11 00:18:54 -0500

    chore: bump version to 1.2.1

--- src/AgentWiki.Core/Constants/AgentWikiConstants.cs
+++ src/AgentWiki.Core/Constants/AgentWikiConstants.cs
@@ -7,7 +7,7 @@ public static class AgentWikiConstants
 {
     public const string ToolName = "agent-wiki";
     public const string ProductName = "AgentWiki";
-    public const string Version = "1.2.0";
+    public const string Version = "1.2.1";
 
     public const string ConfigDirectoryName = ".agentwiki";
     public const string ConfigFileName = "config.json";
